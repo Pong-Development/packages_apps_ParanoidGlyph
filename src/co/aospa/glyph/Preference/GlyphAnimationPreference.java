f061afc544f27b434f32b7f5cde93fd0da2c7ab0
hiroshi <hiroshi@tanuki.page>
2025-09-23 17:24:00 -0400

    ParanoidGlyph: Add support for playing Phone (3a) animations in settings

--- src/co/aospa/glyph/Preference/GlyphAnimationPreference.java
+++ src/co/aospa/glyph/Preference/GlyphAnimationPreference.java
@@ -205,6 +205,12 @@ public class GlyphAnimationPreference extends Preference {
                                     setGlyphsDrawable(animationImgs[9], Integer.parseInt(split[25]));
                                     setGlyphsDrawable(animationImgs[10], Integer.parseInt(split[24]));
                             });
+                        } else if (Constants.getDevice().equals("phone3a") && split.length == 36) { // Phone (3a) / (3a) Pro pattern on Phone (3a) / (3a) Pro
+                            mActivity.runOnUiThread(() -> {
+                                    setGlyphsDrawable(animationImgs[0], Integer.parseInt(split[0]));
+                                    setGlyphsDrawable(animationImgs[1], Integer.parseInt(split[20]));
+                                    setGlyphsDrawable(animationImgs[2], Integer.parseInt(split[31]));
+                            });
                         } else {
                             if (DEBUG) Log.d(TAG, "Animation line length mismatch | name: " + animationName + " | line: " + line);
                             updateAnimation(false);
