458a05f7338e15b003189daebf66066040deb961
hiroshi <hiroshi@tanuki.page>
2026-01-13 11:03:10 -0500

    ParanoidGlyph: Set volume animation as inactive when array is empty
    Without this, reaching 0 volume wouldn't properly dismiss, preventing progess animations from displaying

--- src/co/aospa/glyph/Manager/AnimationManager.java
+++ src/co/aospa/glyph/Manager/AnimationManager.java
@@ -302,8 +302,10 @@ public final class AnimationManager {
         int[] emptyArray = new int[ResourceUtils.getInteger("glyph_settings_volume_levels_num")];
         int[] volumeArray = StatusManager.getVolumeArray();
 
-        if (Arrays.equals(emptyArray, volumeArray))
+        if (Arrays.equals(emptyArray, volumeArray)) {
+            StatusManager.setVolumeAnimationActive(false);
             return;
+        }
 
         if (!check("Dismiss volume", false))
             return;
