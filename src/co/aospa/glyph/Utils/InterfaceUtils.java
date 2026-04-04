50bfcb6f5a1a40768ac3ddd89ae7cb5269d42480
hiroshi <hiroshi@tanuki.page>
2026-04-04 06:15:58 -0400

    ParanoidGlyph: InterfaceUtils: Add a view implementation of showDialog
    We can attach a view to dialogs as well instead of text if it gets too long

--- src/co/aospa/glyph/Utils/InterfaceUtils.java
+++ src/co/aospa/glyph/Utils/InterfaceUtils.java
@@ -4,6 +4,7 @@ import static co.aospa.glyph.Constants.Constants.CONTEXT;
 
 import android.app.AlertDialog;
 import android.content.Context;
+import android.view.View;
 import android.widget.Toast;
 
 public class InterfaceUtils {
@@ -43,10 +44,39 @@ public class InterfaceUtils {
     public static <T> void showDialog(Context ctx, T title, T message,
                                       T positiveText, Runnable onPositive,
                                       T negativeText, Runnable onNegative) {
-       showDialog(ctx, title, message,
-               positiveText, onPositive,
-               negativeText, onNegative,
-               (String) null, null);
+        showDialog(ctx, title, message,
+                positiveText, onPositive,
+                negativeText, onNegative,
+                (String) null, null);
     }
+
+    public static <T> void showDialog(Context ctx, T title, View messageVew,
+                                      T positiveText, Runnable onPositive,
+                                      T negativeText, Runnable onNegative,
+                                      T neutralText, Runnable onNeutral) {
+        new AlertDialog.Builder(ctx)
+                .setTitle(resolve(title))
+                .setView(messageVew)
+                .setPositiveButton(resolve(positiveText), (dialog, which) -> {
+                    if (onPositive != null) onPositive.run();
+                })
+                .setNeutralButton(resolve(neutralText), (dialog, which) -> {
+                    if (onNeutral != null) onNeutral.run();
+                })
+                .setNegativeButton(resolve(negativeText), (dialog, which) -> {
+                    if (onNegative != null) onNegative.run();
+                })
+                .show();
+    }
+
+    public static <T> void showDialog(Context ctx, T title, View messageView,
+                                      T positiveText, Runnable onPositive,
+                                      T negativeText, Runnable onNegative) {
+        showDialog(ctx, title, messageView,
+                positiveText, onPositive,
+                negativeText, onNegative,
+                (String) null, null);
+    }
+
 
 }
